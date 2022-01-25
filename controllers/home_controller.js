580282e2847fddaca9429a3a0b273bdbeb269a68
Ishita <ishkatyal16@gmail.com>
2022-01-25 16:04:49 +0530

    simplifying the code

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,26 +1,40 @@
 const Post = require('../models/post');
+const User = require('../models/user');
 
-module.exports.home = function(req, res){
+module.exports.home = async function(req, res){
     // console.log(req.cookies);
     // res.cookie('user_id', 25);
 
-     
-    // populate the user of each post
-    Post.find({})
-    .populate('user')
-    .populate({
-        path:'comments',
-        populate: {
+    try{
+         // populate the user of each post
+        let posts= await Post.find({})
+        .populate('user')
+        .populate({
+            path:'comments',
+            populate: {
             path: 'user'
-        }
-    })
-    .exec(function(err, posts){
-        return res.render('home', {
-           title: "Codeial | Home",
-            posts:  posts
-       });
-    })
+            }
+        });
 
+        let users = await User.find({});
+    
+        return res.render('home', {
+            title: "Codeial | Home",
+            posts:  posts,
+            all_users: users
+
+    
+         });
+    
+
+    }catch(err){
+     
+        console.log('Error', err);
+        return;
+     }
+    
+        
+   
 }
 
 // module.exports.actionName = function(req, res){}
